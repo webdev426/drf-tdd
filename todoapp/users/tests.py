731a03f663d0f9ba3200849c91f7e3d698524638
erdem <erdemozkol@gmail.com>
2016-03-03 23:53:37 +0200

    user registration unit tests checking, user can get already exists usernames

--- todoapp/users/tests.py
+++ todoapp/users/tests.py
@@ -1,4 +1,5 @@
 import json
+from django.contrib.auth.models import User
 from django.test import TestCase
 
 from rest_framework.test import APIRequestFactory
@@ -11,7 +12,7 @@ class UserRegistrationAPIViewTestCase(TestCase):
 
     def test_invalid_password(self):
         """
-        Test to verify that a post call with invalid passwords returns a 400 status code
+        Test to verify that a post call with invalid passwords
         """
         user_data = {
             "username": "testuser",
@@ -25,6 +26,9 @@ class UserRegistrationAPIViewTestCase(TestCase):
         self.assertEqual(400, response.status_code)
 
     def test_user_registration(self):
+        """
+        Test to verify that a post call with user valid data
+        """
         user_data = {
             "username": "testuser",
             "email": "test@testuser.com",
@@ -36,5 +40,27 @@ class UserRegistrationAPIViewTestCase(TestCase):
         response = view(request)
         response.render()
         self.assertEqual(201, response.status_code)
-        print response.content
         self.assertTrue("token" in json.loads(response.content))
+
+    def test_unique_username_validation(self):
+        user_data_1 = {
+            "username": "testuser",
+            "email": "test@testuser.com",
+            "password": "123123",
+            "confirm_password": "123123"
+        }
+        request = self.factory.post('/api/users/', user_data_1)
+        view = views.UserRegistrationAPIView.as_view()
+        response = view(request)
+        self.assertEqual(201, response.status_code)
+
+        user_data_2 = {
+            "username": "testuser",
+            "email": "test2@testuser.com",
+            "password": "123123",
+            "confirm_password": "123123"
+        }
+        request = self.factory.post('/api/users/', user_data_2)
+        view = views.UserRegistrationAPIView.as_view()
+        response = view(request)
+        self.assertEqual(400, response.status_code)
